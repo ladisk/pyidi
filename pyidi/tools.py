3c7782a3a6c1b0ce64a76c309a4e3cb98b8eda6a
klemen_git <zaletelj.klemen@gmail.com>
2019-04-26 11:02:48 +0200

    tools.py manual ROI selection

--- pyidi/tools.py
+++ pyidi/tools.py
@@ -182,7 +182,109 @@ class RegularROIGrid:
                 fill=False, color='r', linewidth=2))
 
 
+class ManualROI:
+    """Manual ROI selection."""
 
+    def __init__(self, video, roi_size, single=False, verbose=0):
+        """Manually select region of interest.
+        
+        :param video: parent object
+        :type video: object
+        :param roi_size: size of region of interest (dy, dx)
+        :type roi_size: tuple, list
+        :param single: if True, ONLY ONE ROI can be selected, defaults to False
+        :type single: bool, optional
+        :param verbose: Show text, defaults to 0
+        :type verbose: int, optional
+        """
+        self.roi_size = roi_size
+        self.image = video.mraw[0]
+        self.verbose = verbose
+
+        # Tkinter root and matplotlib figure
+        root = tk.Tk()
+        root.title('Pick point')
+        fig = Figure(figsize=(15, 7))
+        ax = fig.add_subplot(111)
+        ax.grid(False)
+        ax.imshow(self.image, cmap='gray')
+        plt.show()
+
+        # Embed figure in tkinter winodw
+        canvas = FigureCanvasTkAgg(fig, root)
+        canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
+        NavigationToolbar2Tk(canvas, root)
+
+        if self.verbose:
+            print('SHIFT + LEFT mouse button to pick a pole.\nRIGHT mouse button to erase the last pick.')
+
+
+        self.point = [[], []]
+        line, = ax.plot(self.point[1], self.point[0], 'r.')
+        
+        self.rectangles = []
+        self.rectangles.append(patches.Rectangle((0, 0), 10, 10, fill=False, alpha=0))
+        ax.add_patch(self.rectangles[-1])        
+        
+        self.shift_is_held = False
+        def on_key_press(event):
+            """Function triggered on key press (shift)."""
+            if event.key == 'shift':
+                self.shift_is_held = True
+
+        def on_key_release(event):
+            """Function triggered on key release (shift)."""
+            if event.key == 'shift':
+                self.shift_is_held = False
+
+        def onclick(event):
+            if event.button == 1 and self.shift_is_held:
+                if event.xdata is not None and event.ydata is not None:
+                    if single:
+                        self.point[0] = [int(np.round(event.ydata))]
+                        self.point[1] = [int(np.round(event.xdata))]
+                    else:
+                        self.point[0].append(int(np.round(event.ydata)))
+                        self.point[1].append(int(np.round(event.xdata)))
+                    if self.verbose:
+                        print(f'y: {np.round(event.ydata):5.0f}, x: {np.round(event.xdata):5.0f}')
+
+            elif event.button == 3 and self.shift_is_held and not single:
+                if self.verbose:
+                    print('Deleted the last point...')
+                del self.point[1][-1]
+                del self.point[0][-1]
+                del self.rectangles[-1]
+
+            line.set_xdata(self.point[1])
+            line.set_ydata(self.point[0])
+            
+            if self.point[0]:
+                [p.remove() for p in reversed(ax.patches)]
+                self.rectangles = []
+                for i, (p0, p1) in enumerate(zip(self.point[0], self.point[1])):
+                    self.rectangles.append(patches.Rectangle((p1 - self.roi_size[1]//2, p0 - self.roi_size[0]//2), 
+                                                    self.roi_size[1], self.roi_size[0], fill=False, color='r', linewidth=2))
+                    ax.add_patch(self.rectangles[-1])
+
+            fig.canvas.draw()
+
+        def handle_close(event):
+            """On closing."""
+            self.points = np.asarray(self.point).T
+            if self.verbose:
+                for i, point in enumerate(self.polygon):
+                    print(f'{i+1}. point: x ={point[1]:5.0f}, y ={point[0]:5.0f}')
+
+        # Connecting functions to event manager
+        fig.canvas.mpl_connect('key_press_event', on_key_press)
+        fig.canvas.mpl_connect('key_release_event', on_key_release)
+        fig.canvas.mpl_connect('button_press_event', onclick)
+        # on closing the figure
+        fig.canvas.mpl_connect('close_event', handle_close)
+
+        root.mainloop()
+    
 
 def inside_polygon(x, y, points):
     """Return True if a coordinate (x, y) is inside a polygon defined by
