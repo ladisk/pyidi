0e29cedfa795a424c39392751fd521f075f58d91
Krištof Čufar <kristof.cufar@gmail.com>
2023-09-08 12:31:56 +0200

    Applied development to _motion_magnification.py

--- pyidi/postprocessing/_motion_magnification.py
+++ pyidi/postprocessing/_motion_magnification.py
@@ -29,7 +29,91 @@ def motion_magnification(video, disp, mag_fact):
     shell_def = shell.warp_by_vector(vectors = "vectors", factor = mag_fact)
 
     # Initialize the output image
-    img_out = video.mraw[0] * 0.5
+    img_out, a, b = init_output_image(video.mraw[0], video.points, shell_def)
+
+    # Element-wise image warping
+    for i in range(shell.n_cells):
+        el_0 = np.float32(shell.cell_points(i)[:,:2])
+        el_1 = np.float32(shell_def.cell_points(i)[:,:2])
+
+        rect_0 = cv.boundingRect(el_0)
+        rect_1 = cv.boundingRect(el_1)
+
+        reg_0 = [((el_0[j, 0] - rect_0[0]), 
+                    (el_0[j, 1] - rect_0[1])) 
+                    for j in range(3)]
+
+        reg_1 = [((el_1[j, 0] - rect_1[0]), 
+                    (el_1[j, 1] - rect_1[1])) 
+                    for j in range(3)]
+
+        crop_0 = video.mraw[0][rect_0[1] : rect_0[1] + rect_0[3],
+                        rect_0[0] : rect_0[0] + rect_0[2]]
+
+        aff_mat = cv.getAffineTransform(
+            src = np.float32(reg_0),
+            dst = np.float32(reg_1)
+        )
+
+        crop_1 = cv.warpAffine(
+            src = crop_0,
+            M = aff_mat,
+            dsize = (rect_1[2], rect_1[3]),
+            dst = None,
+            flags = cv.INTER_LINEAR,
+            borderMode = cv.BORDER_REFLECT_101,
+        )
+
+        mask = np.zeros((rect_1[3], rect_1[2]), dtype=np.float32)
+        mask = cv.fillConvexPoly(
+            img = mask,
+            points = np.int32(reg_1),
+            color = 1,
+            lineType = cv.LINE_AA,
+            shift=0
+        )
+
+        img_out[
+            rect_1[1] + a : rect_1[1] + rect_1[3] + a,
+            rect_1[0] + b : rect_1[0] + rect_1[2] + b
+        ] = img_out[
+            rect_1[1] + a : rect_1[1] + rect_1[3] + a,
+            rect_1[0] + b : rect_1[0] + rect_1[2] + b
+        ] * (1.0 - mask) + crop_1 * mask
+
+    return img_out
+
+
+def init_output_image(input_image, coord, vect):
+    # Find the dimensions of the input image
+    input_height, input_width = input_image.shape[:2]
+
+    # Calculate the distances between mesh nodes and image edges
+    distances = np.array([
+        coord[:, 1],           # Distances to the left edge
+        coord[:, 0],           # Distances to the top edge
+        input_width - coord[:, 1],  # Distances to the right edge
+        input_height - coord[:, 0]  # Distances to the bottom edge
+    ])
     
-    # element-wise transformation
+    # Calculate the minimum distance from the edges
+    min_distance = np.min(distances)
+    
+    # Calculate the minimum and maximum of the deformed mesh nodes
+    min_x = np.min(vect.points[:,0])
+    max_x = np.max(vect.points[:,0])
+    min_y = np.min(vect.points[:,1])
+    max_y = np.max(vect.points[:,1])
+    
+    # Calculate the new size for the output image based on the minimum distance and mesh node coordinates
+    new_width = int(max_x - min_x + 2 * min_distance)
+    new_height = int(max_y - min_y + 2 * min_distance)
+
+    dy = int(abs(min_distance - min_y))
+    dx = int(abs(min_distance - min_x))
+
+    out = np.ones((new_height, new_width)) * np.average(input_image) * 0.3
+    # out[dy : dy + input_height, dx : dx + input_width] = input_image * 0.3
+    
+    return out, dy, dx
     
