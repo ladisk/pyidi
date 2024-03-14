04d19e37dff2c7fab6497cec096ba23dd4602789
klemengit <zaletelj.klemen@gmail.com>
2024-03-14 15:50:22 +0100

    added readthedocs.yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
