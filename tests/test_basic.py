e8ac912d11d741863a46428a4d83eb0fdc76a4d3
Ivan Tomac <itomac@fesb.hr>
2025-05-11 20:47:55 +0200

    Further tests and upgrades are introduced. * `test_basic.py` has been updated to the new API * `test_reader.py` new tests have been introduced for reading `mp4` and `np.ndarray` files * `test_npndarray.py` is removed as this functionality is tested in `test_reader.py` * `data\data_synthetic.mp4` has added mp4 video file

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,20 +6,31 @@ sys.path.insert(0, my_path + '/../')
 import pyidi
 
 def test_instance():
-    video = pyidi.pyIDI(input_file='./data/data_synthetic.cih')
-    video.set_method(method='sof')
-    video.set_method(method='lk')
+    video = pyidi.VideoReader(input_file='./data/data_synthetic.cih')
+    lk = pyidi.LucasKanade(video)
+    sof = pyidi.SimplifiedOpticalFlow(video)
+
     assert True
+    print('test_instance: passed')
+    return None
 
-def test_points():
-    video = pyidi.pyIDI(input_file='./data/data_synthetic.cih')
-    video.set_method(method='sof')
-    video.set_points(points=[(0, 1), (1, 1)])
+def test_points_sof():
+    video = pyidi.VideoReader(input_file='./data/data_synthetic.cih')
+    sof = pyidi.SimplifiedOpticalFlow(video)
+    sof.set_points(points=[(0, 1), (1, 1)])
 
-def test_points_1():
-    video = pyidi.pyIDI(input_file='./data/data_synthetic.cih')
-    video.set_method(method='sof')
-    video.set_points(points=[(0, 1), (1, 1)])
+    assert True
+    print('test_points_sof: passed')
+    return None
+
+def test_points_lk():
+    video = pyidi.VideoReader(input_file='./data/data_synthetic.cih')
+    lk = pyidi.LucasKanade(video)
+    lk.set_points(points=[(0, 1), (1, 1)])
+
+    assert True
+    print('test_points_lk: passed')
+    return None
 
 # def test_info():
 #     video = pyidi.pyIDI(input_file='./data/data_synthetic.cih')
@@ -29,8 +40,10 @@ def test_points_1():
 #     assert 'Record Rate(fps)' in video.reader.info.keys()
 
 
-# if __name__ == '__main__':
-#     test_info()
+if __name__ == '__main__':
+    test_instance()
+    test_points_sof()
+    test_points_lk()
 
-if __name__ == '__mains__':
-    np.testing.run_module_suite()
+# if __name__ == '__mains__':
+#     np.testing.run_module_suite()
--- tests/test_reader/test_reader.py
+++ tests/test_reader/test_reader.py
@@ -1,12 +1,13 @@
-import numpy as np
-from numpy.testing import assert_array_equal
 import sys, os
+import pyMRAW
+import numpy as np
+
 my_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, my_path + '/../../')
 
 import pyidi
 
-def test_1():
+def test_png_stream():
     video = pyidi.VideoReader(input_file='./data/data_synthetic_img_0.png')
 
     assert video.file_format == 'png'
@@ -14,6 +15,31 @@ def test_1():
     assert hasattr(video, "image_width")
     assert hasattr(video, "image_height")
     assert hasattr(video, "fps")
+    # print('test_png_stream: passed')
+    return None
+
+def test_mp4():
+    video = pyidi.VideoReader(input_file='./data/data_synthetic.mp4')
+
+    assert video.file_format == 'mp4'
+    assert hasattr(video, "N")
+    assert hasattr(video, "image_width")
+    assert hasattr(video, "image_height")
+    assert hasattr(video, "fps")
+    # print('test_mp4: passed')
+    return None
+
+def test_ndarray():
+    data, info = pyMRAW.load_video('./data/data_showcase.cih')
+    video = pyidi.VideoReader(data, root='./data/', fps=info.get('Record Rate(fps)'))
+
+    assert video.file_format == 'np.ndarray'
+    assert hasattr(video, "N")
+    assert hasattr(video, "image_width")
+    assert hasattr(video, "image_height")
+    assert hasattr(video, "fps")
+    # print('test_ndarray: passed')
+    return None
 
 def test_get_frames():
     video = pyidi.VideoReader(input_file='./data/data_synthetic_img_0.png')
@@ -21,6 +47,8 @@ def test_get_frames():
     assert video.get_frames().shape[0] == 10
     assert video.get_frames(4).shape[0] == 4
     assert video.get_frames((1, 5)).shape[0] == 4
+    # print('test_get_frames: passed')
+    return None
 
 def test_get_frames_mraw():
     video = pyidi.VideoReader(input_file='./data/data_showcase.cih')
@@ -28,3 +56,37 @@ def test_get_frames_mraw():
     assert video.get_frames().shape[0] == 75
     assert video.get_frames(4).shape[0] == 4
     assert video.get_frames((1, 5)).shape[0] == 4
+    # print('test_get_frames_mraw: passed')
+    return None
+
+def test_get_frames_ndarray():
+    data, info = pyMRAW.load_video('./data/data_showcase.cih')
+    video = pyidi.VideoReader(data, root='./data/', fps=info.get('Record Rate(fps)'))
+
+    assert video.get_frames().shape[0] == 75
+    assert video.get_frames(4).shape[0] == 4
+    assert video.get_frames((1, 5)).shape[0] == 4
+    # print('test_get_frames_ndarray: passed')
+    return None
+
+
+def test_get_frames_mp4():
+    video = pyidi.VideoReader(input_file='./data/data_synthetic.mp4')
+
+    assert video.get_frames().shape[0] == 10
+    assert video.get_frames(use_channel='R').shape[0] == 10
+    assert video.get_frames(use_channel='G').shape[0] == 10
+    assert video.get_frames(use_channel='B').shape[0] == 10
+    assert video.get_frames(4).shape[0] == 4
+    assert video.get_frames((1, 5)).shape[0] == 4
+    # print('test_get_frames_mp4: passed')
+    return None
+
+if __name__ == '__main__':
+    test_png_stream()
+    test_mp4()
+    test_ndarray()
+    test_get_frames()
+    test_get_frames_mraw()
+    test_get_frames_ndarray()
+    test_get_frames_mp4()
