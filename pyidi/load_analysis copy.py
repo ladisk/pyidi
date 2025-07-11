e54189d3d38d800dd706ac457071027e2836545f
Klemen Zaletelj <zaletelj.klemen@gmail.com>
2025-07-11 15:01:47 +0200

    added `root` argument in `load_analysis` (relevant when loading analysis done on `np.array` input)

---
+++ pyidi/load_analysis copy.py
@@ -0,0 +1,58 @@
+import os
+import json
+import pickle
+import warnings
+
+from .methods import LucasKanade, SimplifiedOpticalFlow, DirectionalLucasKanade, IDIMethod
+from .video_reader import VideoReader
+
+method_mappings = {
+    "LucasKanade": LucasKanade,
+    "SimplifiedOpticalFlow": SimplifiedOpticalFlow,
+    "DirectionalLucasKanade": DirectionalLucasKanade,
+}
+
+def load_analysis(analysis_path, input_file=None, load_results=True, root=None):
+    """Load the previous analysis and create a pyIDI object.
+
+    :param analysis_path: Path to analysis folder (e.g. video_pyidi_analysis/analysis_001/)
+    :type analysis_path: str
+    :param input_file: new location of the cih file, if None, the location in settings.txt 
+        is used, defaults to None
+    :type input_file: str or None, optional
+    :param load_results: if False, the displacements are not loaded,
+        only points and settings, defaults to True
+    :type load_results: bool, optional
+    :param root: root directory for the analysis (needed when the ``VideoReader`` requires it), 
+        defaults to None.
+    :type root: str or None, optional
+    :return: pyIDI object and settings dict
+    :rtype: tuple
+    """
+    with open(os.path.join(analysis_path, 'settings.json'), 'r') as f:
+        settings = json.load(f)
+    
+    if input_file is None:
+        video = VideoReader(settings['input_file'], root=root)
+    else:
+        video = VideoReader(input_file, root=root)
+
+    method_name = settings['method']
+    if method_name not in method_mappings:
+        raise ValueError(f"Method {method_name} not one of {list(method_mappings.keys())}")
+    
+    idi: IDIMethod = method_mappings[method_name](video)
+    
+    with open(os.path.join(analysis_path, 'points.pkl'), 'rb') as f:
+        points = pickle.load(f)
+
+    if load_results:
+        with open(os.path.join(analysis_path, 'results.pkl'), 'rb') as f:
+            results = pickle.load(f)
+            
+        idi.displacements = results
+
+    idi.set_points(points)
+
+    return video, idi, settings['settings']
+
