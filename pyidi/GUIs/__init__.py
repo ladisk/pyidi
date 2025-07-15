c2bb5c23bb20a75a76f7aa3af15eff74ea4d29f3
Klemen Zaletelj <zaletelj.klemen@gmail.com>
2025-07-15 14:00:22 +0200

    Fixed dummy QT gui classes; added pyQT6 and pyqtgraph to optional dependencies

--- pyidi/GUIs/__init__.py
+++ pyidi/GUIs/__init__.py
@@ -12,18 +12,12 @@ if HAS_PYQT6 or typing.TYPE_CHECKING:
     from .result_viewer import ResultViewer
 else:
     class SelectionGUI:
-        def __init__(self, video):
-            pass
-            
-        def show_displacement(self, data):
+        def __init__(self, *args, **kwargs):
             raise RuntimeError("SelectionGUI requires PyQt6: pip install pyidi[qt]")
-    
+
     class ResultViewer:
-        def __init__(self):
-            pass
-            
-        def show_displacement(self, data):
+        def __init__(self, *args, **kwargs):
             raise RuntimeError("ResultViewer requires PyQt6: pip install pyidi[qt]")
-        
+
 from .selection import SubsetSelection
 from .gui import GUI
